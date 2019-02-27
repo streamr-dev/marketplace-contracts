70595ad1e58146dadda384cbbc9bf991eea64f2c
Jonathan Wolff <jwolff@isentropy.com>
2019-02-27 15:09:52 +0100

    pragma

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.22;
+// solhint-disable not-rely-on-time
+pragma solidity 0.4.25;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
