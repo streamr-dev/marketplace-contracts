a3ca4524b6702759f704aed6455b91c1886d2907
Jonathan Wolff <jwolff@isentropy.com>
2019-03-14 18:42:12 +0100

    uses real uniswap contracts

--- contracts/UniswapAdaptor.sol
+++ contracts/UniswapAdaptor.sol
@@ -31,14 +31,65 @@ contract IERC20Token {
     function approve(address _spender, uint256 _value) public returns (bool success);
 }
 
-contract IUniswapFactory {
-    function getExchange(address token) public returns (address){}
-}
 
+//from https://docs.uniswap.io/smart-contract-integration/interface:
+contract IUniswapFactory {
+    // Public Variables
+    address public exchangeTemplate;
+    uint256 public tokenCount;
+    // Create Exchange
+    function createExchange(address token) external returns (address exchange);
+    // Get Exchange and Token Info
+    function getExchange(address token) external view returns (address exchange);
+    function getToken(address exchange) external view returns (address token);
+    function getTokenWithId(uint256 tokenId) external view returns (address token);
+    // Never use
+    function initializeFactory(address template) external;
+}
 contract IUniswapExchange{
-    function getEthToTokenOutputPrice(uint256 tokens_bought) public view returns (uint256) {}
-    function ethToTokenTransferInput(uint256 min_tokens,uint deadline,address recipient) public payable returns (uint256) {}
+    // Address of ERC20 token sold on this exchange
+    function tokenAddress() external view returns (address token);
+    // Address of Uniswap Factory
+    function factoryAddress() external view returns (address factory);
+    // Provide Liquidity
+    function addLiquidity(uint256 min_liquidity, uint256 max_tokens, uint256 deadline) external payable returns (uint256);
+    function removeLiquidity(uint256 amount, uint256 min_eth, uint256 min_tokens, uint256 deadline) external returns (uint256, uint256);
+    // Get Prices
+    function getEthToTokenInputPrice(uint256 eth_sold) external view returns (uint256 tokens_bought);
+    function getEthToTokenOutputPrice(uint256 tokens_bought) external view returns (uint256 eth_sold);
+    function getTokenToEthInputPrice(uint256 tokens_sold) external view returns (uint256 eth_bought);
+    function getTokenToEthOutputPrice(uint256 eth_bought) external view returns (uint256 tokens_sold);
+    // Trade ETH to ERC20
+    function ethToTokenSwapInput(uint256 min_tokens, uint256 deadline) external payable returns (uint256  tokens_bought);
+    function ethToTokenTransferInput(uint256 min_tokens, uint256 deadline, address recipient) external payable returns (uint256  tokens_bought);
+    function ethToTokenSwapOutput(uint256 tokens_bought, uint256 deadline) external payable returns (uint256  eth_sold);
+    function ethToTokenTransferOutput(uint256 tokens_bought, uint256 deadline, address recipient) external payable returns (uint256  eth_sold);
+    // Trade ERC20 to ETH
+    function tokenToEthSwapInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline) external returns (uint256  eth_bought);
+    function tokenToEthTransferInput(uint256 tokens_sold, uint256 min_tokens, uint256 deadline, address recipient) external returns (uint256  eth_bought);
+    function tokenToEthSwapOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline) external returns (uint256  tokens_sold);
+    function tokenToEthTransferOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline, address recipient) external returns (uint256  tokens_sold);
+    // Trade ERC20 to ERC20
+    function tokenToTokenSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address token_addr) external returns (uint256  tokens_bought);
     function tokenToTokenTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_bought);
+    function tokenToTokenSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address token_addr) external returns (uint256  tokens_sold);
+    function tokenToTokenTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_sold);
+    // Trade ERC20 to Custom Pool
+    function tokenToExchangeSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address exchange_addr) external returns (uint256  tokens_bought);
+    function tokenToExchangeTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_bought);
+    function tokenToExchangeSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address exchange_addr) external returns (uint256  tokens_sold);
+    function tokenToExchangeTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_sold);
+    // ERC20 comaptibility for liquidity tokens
+    bytes32 public name;
+    bytes32 public symbol;
+    uint256 public decimals;
+    function transfer(address _to, uint256 _value) external returns (bool);
+    function transferFrom(address _from, address _to, uint256 value) external returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
+    function allowance(address _owner, address _spender) external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256);
+    // Never use
+    function setup(address token_addr) external;
 }
 
 contract UniswapAdaptor {
@@ -65,6 +116,33 @@ contract UniswapAdaptor {
 
         return c;
     }
+    /*
+        token address 0x0 means ETH
+    */
+    
+    function getConversionRate(address from_token, address to_token, uint amount) public view returns (uint){
+        require(from_token != to_token, "must specify different tokens ");
+        uint eth_amount;
+        if(from_token == 0x0){
+            eth_amount = amount;
+        }
+        else{
+            address from_token_exchange = uniswap_factory.getExchange(from_token);
+            require(from_token_exchange != 0x0, "couldnt find exchange for from_token");
+            IUniswapExchange exfrom = IUniswapExchange(from_token_exchange);
+            eth_amount = exfrom.getTokenToEthInputPrice(amount);
+        }
+        if(to_token == 0x0){
+            return eth_amount;
+        }
+        else{
+            address to_token_exchange = uniswap_factory.getExchange(to_token);
+            require(to_token_exchange != 0x0, "couldnt find exchange for to_token");
+            IUniswapExchange exto = IUniswapExchange(to_token_exchange);
+            return exto.getEthToTokenInputPrice(eth_amount);
+        }
+    }
+    
 
     function _getPricePerSecondData(bytes32 productId) internal view returns (uint) {
         (, address owner,, uint pricePerSecond, IMarketplace.Currency priceCurrency,,) = marketplace.getProduct(productId);
--- test/UniswapAdaptor.js
+++ test/UniswapAdaptor.js
@@ -1,3 +1,7 @@
+var fs = require('fs');
+const Web3 = require("web3")
+const w3 = new Web3(web3.currentProvider)
+
 const Marketplace_prev = artifacts.require("./Marketplace_20180425.sol")
 const Marketplace = artifacts.require("./Marketplace.sol")
 const UniswapAdaptor = artifacts.require("./UniswapAdaptor.sol")
@@ -5,14 +9,22 @@ const ERC20Mintable = artifacts.require("zeppelin-solidity/contracts/token/ERC20
 const MockUniswapFactory = artifacts.require("./MockUniswapFactory.sol")
 const MockUniswapExchange = artifacts.require("./MockUniswapExchange.sol")
 const { Marketplace: { ProductState, Currency } } = require("../src/contracts/enums")
-const Web3 = require("web3")
-const w3 = new Web3(web3.currentProvider)
 
 const { assertEvent, assertEqual, assertFails, assertEventBySignature, now } = require("./testHelpers")
 
 const paths = require("../currencydata")
 const bancor = require("../src/bancor")
 
+//from https://github.com/Uniswap/contracts-vyper
+const uniswap_exchange_abi = JSON.parse(fs.readFileSync('./build/abi/uniswap_exchange.json', 'utf-8'));
+const uniswap_factory_abi = JSON.parse(fs.readFileSync('./build/abi/uniswap_factory.json', 'utf-8'));
+const uniswap_exchange_bytecode = fs.readFileSync('./build/bytecode/uniswap_exchange.txt', 'utf-8');
+const uniswap_factory_bytecode = fs.readFileSync('./build/bytecode/uniswap_factory.txt', 'utf-8');
+
+const UniswapFactory = new w3.eth.Contract(uniswap_factory_abi, null, { data: uniswap_factory_bytecode });
+const UniswapExchange = new w3.eth.Contract(uniswap_exchange_abi, null, { data: uniswap_exchange_bytecode });
+const futureTime = 4449513600;
+//console.log(uniswap_exchange_abi);
 contract("UniswapAdaptor", accounts => {
     let market
     let fromToken
@@ -26,21 +38,36 @@ contract("UniswapAdaptor", accounts => {
     const streamOwner = accounts[3]
     const productId = "0x123"
     const testToleranceSeconds = 5
-    const day=86400
+    const day = 86400
 
     before(async () => {
         fromToken = await ERC20Mintable.new({ from: creator })
         dataToken = await ERC20Mintable.new({ from: creator })
-        uniswapExchange = await MockUniswapExchange.new(fromToken.address,dataToken.address,{ from: creator } )
-        uniswapFactory = await MockUniswapFactory.new(uniswapExchange.address,{ from: creator } )
+        await dataToken.mint(creator, w3.utils.toWei("100000000"), { from: creator })
+        await fromToken.mint(creator, w3.utils.toWei("100000000"), { from: creator })
+        await fromToken.mint(buyer, w3.utils.toWei("100000000"), { from: creator })
+        uniswapFactory = await UniswapFactory.deploy(({ arguments: [] })).send({ gas: 6000000, from: creator });
+        const templateExchange = await UniswapExchange.deploy(({ arguments: [] })).send({ gas: 6000000, from: creator });
+        await uniswapFactory.methods.initializeFactory(templateExchange.options.address).send({ gas: 6000000, from: creator });
+        await uniswapFactory.methods.createExchange(fromToken.address).send({ gas: 6000000, from: creator });
+        await uniswapFactory.methods.createExchange(dataToken.address).send({ gas: 6000000, from: creator });
+        let fromtoken_exchange = await uniswapFactory.methods.getExchange(fromToken.address).call();
+        let datatoken_exchange = await uniswapFactory.methods.getExchange(dataToken.address).call();
+        const fromTokenUniswapExchange = new w3.eth.Contract(uniswap_exchange_abi, fromtoken_exchange, { data: uniswap_exchange_bytecode });
+        const dataTokenUniswapExchange = new w3.eth.Contract(uniswap_exchange_abi, datatoken_exchange, { data: uniswap_exchange_bytecode });
         market_prev = await Marketplace_prev.new(dataToken.address, currencyUpdateAgent, { from: creator })
         market = await Marketplace.new(dataToken.address, currencyUpdateAgent, market_prev.address, { from: creator })
-        uniswapAdaptor = await UniswapAdaptor.new(market.address, uniswapFactory.address, dataToken.address, { from: creator })
-
-        // function createProduct(bytes32 id, string name, address beneficiary, uint pricePerSecond, Currency currency, uint minimumSubscriptionSeconds) public whenNotHalted
-        await market.createProduct(productId, "testproduct", streamOwner, w3.utils.toWei("1"), Currency.DATA, 1, { from: streamOwner })
-        await dataToken.mint(uniswapExchange.address, w3.utils.toWei("100000000"), { from: creator })
-        await fromToken.mint(buyer, w3.utils.toWei("100000000"), { from: creator })
+        uniswapAdaptor = await UniswapAdaptor.new(market.address, uniswapFactory.options.address, dataToken.address, { from: creator })
+        await market.createProduct(productId, "testproduct", streamOwner, w3.utils.toWei(".001"), Currency.DATA, 1, { from: streamOwner })
+        let dtAmount = w3.utils.toWei("10");
+        let ftAmount = w3.utils.toWei("10");
+        assert(await fromToken.approve(fromtoken_exchange, ftAmount, { from: creator }));
+        assert(await dataToken.approve(datatoken_exchange, dtAmount, { from: creator }));
+        assert(await dataToken.approve(templateExchange.options.address, dtAmount, { from: creator }));
+        //10 fromToken ~= 1 ETH 
+        await fromTokenUniswapExchange.methods.addLiquidity(w3.utils.toWei("10"), w3.utils.toWei("10"), futureTime).send({ from: creator, gas: 6000000, value: w3.utils.toWei("1") });
+        //1 dataToken ~= 1 ETH 
+        await dataTokenUniswapExchange.methods.addLiquidity(dtAmount, dtAmount, futureTime).send({ from: creator, gas: 6000000, value: dtAmount });
     })
 
     describe("Check Adaptor", () => {
@@ -51,27 +78,31 @@ contract("UniswapAdaptor", accounts => {
         })
 
         it("too many seconds fails", async () => {
-            var path = [fromToken.address, dataToken.address]
-            // will return 10 data coin, which pays for 10s
-            await assertFails(uniswapAdaptor.buyWithETH(productId, 11, day, { from: buyer, value: w3.utils.toWei("10") }))
+            // will return ~10 data coin, which pays for 10s
+            await assertFails(uniswapAdaptor.buyWithETH(productId, 20, day, { from: buyer, value: w3.utils.toWei(".01") }))
+            await fromToken.approve(uniswapAdaptor.address, 0, { from: buyer })
+            //=.001 eth which pays for about 1s
+            let value =  w3.utils.toWei(".01");
+            await fromToken.approve(uniswapAdaptor.address,value, { from: buyer })
+            await assertFails(uniswapAdaptor.buyWithERC20(productId, 9, day, fromToken.address, value, { from: buyer }))
+
         })
 
         it("can buy product with ETH", async () => {
-            // path[0] is ignored when converting from ETH with MockBancorConverter
-            var path = [fromToken.address, dataToken.address]
             const [validBefore, endtimeBefore] = await market.getSubscription(productId, buyer, { from: buyer })
-            await uniswapAdaptor.buyWithETH(productId, 9, day, { from: buyer, value: w3.utils.toWei("10") })
+            //.01 eth pays for about 10s
+            await uniswapAdaptor.buyWithETH(productId, 9, day, { from: buyer, value: w3.utils.toWei(".01") })
             const [validAfter, endtimeAfter] = await market.getSubscription(productId, buyer, { from: buyer })
             assert(validAfter)
             assert(endtimeAfter - endtimeBefore > 10 - testToleranceSeconds)
         })
 
         it("can buy product with ERC20", async () => {
-            var path = [fromToken.address, dataToken.address]
             const [validBefore, endtimeBefore] = await market.getSubscription(productId, buyer, { from: buyer })
-            var value = w3.utils.toWei("10")
+            //=.01 eth or about 10s
+            var value = w3.utils.toWei("0.1")
             await fromToken.approve(uniswapAdaptor.address, value, { from: buyer })
-            await uniswapAdaptor.buyWithERC20(productId, 9,day,fromToken.address, value, { from: buyer })
+            await uniswapAdaptor.buyWithERC20(productId, 9, day, fromToken.address, value, { from: buyer })
             const [validAfter, endtimeAfter] = await market.getSubscription(productId, buyer, { from: buyer })
             assert(validAfter)
             assert(endtimeAfter - endtimeBefore > 10 - testToleranceSeconds)
