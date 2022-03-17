864f8782795e2a4a2bd9c86d73c7c2c99c0f2076
Tim Oxley <secoif@gmail.com>
2022-03-17 09:22:56 -0400

    Use hdwallet for private key provider. WIP.

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,4 @@
-const PrivateKeyProvider = require("truffle-privatekey-provider")
+const HDWalletProvider = require("@truffle/hdwallet-provider")
 
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
@@ -19,12 +19,12 @@ module.exports = {
         localsidechain: {
             network_id: 8997,
             host: "localhost",
-            provider: new PrivateKeyProvider("0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0", "http://localhost:8546"),
+            provider: () => new HDWalletProvider("0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0", "http://localhost:8546"),
             port: 8546
         },
         polygonMainnet: {
             network_id: 137,
-            provider: new PrivateKeyProvider(process.env.POLYGON_PK || "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0", "http://polygon-rpc.com"),
+            provider: () => new HDWalletProvider(process.env.POLYGON_PK || "0x5e98cce00cff5dea6b454889f359a4ec06b9fa6b88e9d69b86de8e1c81887da0", "http://polygon-rpc.com"),
             host: "polygon-rpc.com",
         }
     }
