020b1f05fe8212adc38d2f0061075002f0506de8
Tim Oxley <secoif@gmail.com>
2022-03-21 12:21:09 -0400

    Downgrade to faster version of truffle, restore transfer ownership behaviour in test.
    Newer versions are slow.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const config = require("@streamr/config")
 
 const Marketplace = artifacts.require("./Marketplace.sol")
 
 module.exports = async (deployer, network) => {
     const streamrUpdaterAddress = "0xb6aA9D2708475fB026a8052E20e63AeA23233613"
-    let datacoinAddress
+    let datacoinAddress = ""
     let marketplaceAddress
     switch (network) {
-    case "mainnet": {
+    case "test": {
+        datacoinAddress = "0x8e3877fe5551f9c14bc9b062bbae9d84bc2f5d4e"
+        marketplaceAddress = "0x0000000000000000000000000000000000000000"
         break
     }
+    // TODO Ethereum mainnet
     case "localsidechain": {
         const chains = config.Chains.load("development")
-        datacoinAddress = chains.streamr.contracts["Token"]
+        datacoinAddress = chains.streamr.contracts.Token
         // ownerAddress = "0x3F2dA479B77cB583C3462577DCD2e89B965fe987"
         marketplaceAddress = "0x0000000000000000000000000000000000000000"
         break
@@ -26,10 +30,13 @@ module.exports = async (deployer, network) => {
     }
     case "maticTest": {
         const chains = config.Chains.load("development")
-        datacoinAddress = chains.streamr.contracts["Token"]
+        datacoinAddress = chains.streamr.contracts.Token
         // ownerAddress = "0x3F2dA479B77cB583C3462577DCD2e89B965fe987"
         marketplaceAddress = "0x0000000000000000000000000000000000000000"
         break
+    }
+    default: {
+        throw new Error(`Unknown network: ${network}`)
     }
     }
 
@@ -37,10 +44,11 @@ module.exports = async (deployer, network) => {
     const tx = await deployer.deploy(Marketplace, datacoinAddress, streamrUpdaterAddress, marketplaceAddress, {
         gasPrice: (new web3.utils.BN(gasPrice)).mul(new web3.utils.BN(1.5))
     })
-    console.log(tx)
 
     const tx2 = await Marketplace.deployed()
-    console.log(tx2)
-
-    //await m.transferOwnership(ownerAddress, { gas: 400000 })
+    if (network === "test") {
+        await tx2.transferOwnership("0x3F2dA479B77cB583C3462577DCD2e89B965fe987", {
+            gas: 400000
+        })
+    }
 }
