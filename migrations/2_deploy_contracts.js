e2e4356f21b8f5b172cc829da07dcef228d94eef
Tim Kevin Oxley <secoif@gmail.com>
2022-03-21 12:27:40 -0400

    localsidechain and polygon mainnet marketplace deploy (#142)
    * localsidechain and polygon mainnet marketplace deploy WIP
    
    * Use hdwallet for private key provider. WIP.
    
    * added polygon testnet env
    
    * Deployment of Polygon Marketplace contract.
    
    * fix(ci): Fix npm version in CI.
    
    * fix(ci): Run CI for all branches.
    
    * fix(ci): Fix solhint linting.
    
    * Downgrade to faster version of truffle, restore transfer ownership behaviour in test.
    
    Newer versions are slow.
    
    Co-authored-by: Christoph-Samuel Pitter <christoph-samuel.pitter@streamr.network>

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,54 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const config = require("@streamr/config")
+
 const Marketplace = artifacts.require("./Marketplace.sol")
-// const Community = artifacts.require("./MockCommunity.sol")
 
 module.exports = async (deployer, network) => {
-    const mainnet = network.indexOf("main") > -1
-    const datacoinAddress = mainnet ? "0x0cf0ee63788a0849fe5297f3407f701e122cc023" : "0x8e3877fe5551f9c14bc9b062bbae9d84bc2f5d4e"
     const streamrUpdaterAddress = "0xb6aA9D2708475fB026a8052E20e63AeA23233613"
-    const ownerAddress = mainnet ? "0x1bb7804d12fa4f70ab63d0bbe8cb0b1992694338" : "0x3F2dA479B77cB583C3462577DCD2e89B965fe987"
-    //there isn't a previous testnet marketplace
-    const marketplaceAddress = mainnet ? "0xa10151d088f6f2705a05d6c83719e99e079a61c1": "0x0000000000000000000000000000000000000000"
-    await deployer.deploy(Marketplace, datacoinAddress, streamrUpdaterAddress, marketplaceAddress, { gas: 6700000 })
-    // await deployer.deploy(Community, Marketplace.deployed().address, { gas: 6000000 })
-    Marketplace.deployed().then(m => m.transferOwnership(ownerAddress, { gas: 400000 }))
+    let datacoinAddress = ""
+    let marketplaceAddress
+    switch (network) {
+    case "test": {
+        datacoinAddress = "0x8e3877fe5551f9c14bc9b062bbae9d84bc2f5d4e"
+        marketplaceAddress = "0x0000000000000000000000000000000000000000"
+        break
+    }
+    // TODO Ethereum mainnet
+    case "localsidechain": {
+        const chains = config.Chains.load("development")
+        datacoinAddress = chains.streamr.contracts.Token
+        // ownerAddress = "0x3F2dA479B77cB583C3462577DCD2e89B965fe987"
+        marketplaceAddress = "0x0000000000000000000000000000000000000000"
+        break
+    }
+    case "polygonMainnet": {
+        const chains = config.Chains.load("production")
+        datacoinAddress = chains.polygon.contracts["DATA-token"]
+        // ownerAddress = "0xC166609238736DDc36D6a4EFd50046ea9a2ad0AD"
+        marketplaceAddress = "0x0000000000000000000000000000000000000000"
+        break
+    }
+    case "maticTest": {
+        const chains = config.Chains.load("development")
+        datacoinAddress = chains.streamr.contracts.Token
+        // ownerAddress = "0x3F2dA479B77cB583C3462577DCD2e89B965fe987"
+        marketplaceAddress = "0x0000000000000000000000000000000000000000"
+        break
+    }
+    default: {
+        throw new Error(`Unknown network: ${network}`)
+    }
+    }
+
+    const gasPrice = await web3.eth.getGasPrice()
+    const tx = await deployer.deploy(Marketplace, datacoinAddress, streamrUpdaterAddress, marketplaceAddress, {
+        gasPrice: (new web3.utils.BN(gasPrice)).mul(new web3.utils.BN(1.5))
+    })
+
+    const tx2 = await Marketplace.deployed()
+    if (network === "test") {
+        await tx2.transferOwnership("0x3F2dA479B77cB583C3462577DCD2e89B965fe987", {
+            gas: 400000
+        })
+    }
 }
